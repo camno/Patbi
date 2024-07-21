a5137346f235ae42d836a54f1f87b69af17c8a48
camno <121708020+camno@users.noreply.github.com>
2024-07-20 21:45:28 -0400

    read the trading stock from file trading_stock.txt

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ async def main():
     bot = TradingBot()
     await bot.connect()
 
-    contract = Contract(symbol='AAPL', secType='STK', exchange='SMART', currency='USD')
+    contract = Contract(symbol = ticker_name, secType='STK', exchange='SMART', currency='USD')
     order = MarketOrder('BUY', 100)
     await bot.trade(contract, order)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    with open('trading_stock.txt', "r") as f:
+        args = f.read().splitlines()
+    asyncio.run(main(ticker_name = args[0]))
